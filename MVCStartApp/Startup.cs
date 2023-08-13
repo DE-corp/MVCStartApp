5d718815d4875cf6da4a6a70880c02dfb3971955
DE-corp <crazyHOPHOP@rambler.ru>
2023-08-13 19:29:18 +0300

    Добавил LoggingMiddleware с выводом запросов в консоль

--- MVCStartApp/Startup.cs
+++ MVCStartApp/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVCStartApp.Middlewares;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,7 +44,10 @@ namespace MVCStartApp
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            
+            // Логирование
+            app.UseMiddleware<LoggingMiddleware>();
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
